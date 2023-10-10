41b150dd22b712a97af29a2e5d1a7221450641e5
csi-Abil <abil.shrestha@cix.csi.cuny.edu>
2023-10-09 21:48:27 -0400

    Seek bar Complete

--- Frontend/data/dummydata.js
+++ Frontend/data/dummydata.js
@@ -18,10 +18,10 @@ export const dummyData= [
     {
         id:'3',
         user_id:'1',
-        author: 'Intouch',
-        title: 'Fighting Inside',
-        song_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/527/fighting-inside-1695081649-krqWFbOLWj.mp3',
-        image_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/527/1000x0/fighting-inside-1695081647-2ho9MiAsYi.jpg'
+        author: 'Zack Merci',
+        title: 'Kadak',
+        song_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/296/kadak-1674522042-I2GI4NB1E7.mp3',
+        image_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/296/1000x0/kadak-1674522040-esYsJUEyaw.jpg'
     },
         {
         id:'4',
@@ -30,5 +30,26 @@ export const dummyData= [
         title: 'Make Me Move',
         song_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/350/make-me-move-feat-karra-tobu-remix-1586950368-YfVZcYw8y1.mp3',
         image_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/350/1000x0/make-me-move-feat-karra-tobu-remix-1586950360-h3YeDN64Mq.jpg'
-    }
+    },
+    {
+        id:'5',
+        user_id:'9',
+        author: 'paul Flint',
+        title: 'Welm Bah',
+        song_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/203/e-welem-bah-1663923642-1hkCHO99lA.mp3',
+        image_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/203/1000x0/e-welem-bah-1663923639-xnRIkAw3nJ.jpg'
+    },
+    {
+        id:'6',
+        user_id:'7',
+        author: 'Zaug',
+        title: '8 Rounds',
+        song_path:'https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/504/8-rounds-1692316863-3vvxI69GWo.mp3',
+        image_path:' https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/001/504/1000x0/8-rounds-1692316860-AfkIvN46qQ.jpg'
+    },
+
+   
+    
+
+    
 ]
